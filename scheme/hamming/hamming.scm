8dcd4a313c65d12165709acb472f367785e51634
Erik Price <github@erikprice.net>
2016-12-14 16:23:38 -0800

    zip is unnecessary here

--- scheme/hamming/hamming.scm
+++ scheme/hamming/hamming.scm
@@ -1,9 +1,7 @@
 (define-module (hamming)
   #:export (hamming-distance))
 
-;; (zip '(1 2 3) '(4 5 6)) => ((1 4) (2 5) (3 6))
-(define (zip a b) (map list a b))
-
 (define (hamming-distance str1 str2)
-  (apply + (map (lambda (a) (if (eq? (car a) (cadr a)) 0 1))
-                (zip (string->list str1) (string->list str2)))))
+  (apply + (map (lambda (a b) (if (eq? a b) 0 1))
+                (string->list str1)
+                (string->list str2))))
