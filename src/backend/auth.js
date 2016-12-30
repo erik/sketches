a25b22c5fce0133f4771986a37b66af5166fbcbc
Erik Price <github@erikprice.net>
2016-12-30 15:05:26 -0800

    Start working on using knex / sqlite

--- src/backend/auth.js
+++ src/backend/auth.js
@@ -4,6 +4,7 @@ import {Strategy as GoogleStrategy} from 'passport-google-oauth20'
 import {Router} from 'express'
 
 import DATABASE from './database'
+import {findUser, createUser} from './database'
 
 
 let router = Router()
@@ -13,22 +14,29 @@ let secrets = require('../../config/secrets.json')
 
 
 passport.serializeUser((user, done) => done(null, user.id))
-passport.deserializeUser((id, done) => done(null, DATABASE.users[id]))
+passport.deserializeUser((id, done) => findUser({id})
+                         .then(rows => done(null, rows[0]))
+                         .catch(err => done(err, false)))
 
 passport.use(new GoogleStrategy({
   clientID: secrets.google.client_id,
   clientSecret: secrets.google.client_secret,
   callbackURL: secrets.google.callback_url
 }, (token, refreshToken, profile, done) => {
-  let user = DATABASE.users[profile.id] = {
-    id: profile.id,
-    token: token,
-    name: profile.displayName
-  }
+  findUser({google_id: profile.id})
+    .then(rows => {
+      if (rows.length === 0)
+        createUser({
+          google_id: profile.id,
+          google_token: token,
+          name: profile.displayName
+        })
+        .then(user => done(null, user))
+        .catch(err => done(err, false))
 
-  console.log('got my user', user)
-
-  return done(null, user)
+      done(null, rows[0])
+    })
+    .catch(err => done(err, false))
 }))
 
 
--- src/backend/database.js
+++ src/backend/database.js
@@ -1,3 +1,73 @@
+import knexConn from 'knex'
+
+let knex = knexConn({
+  client: 'sqlite3',
+  connection: { filename: '/tmp/database.sqlite' },
+  debug: true,
+  useNullAsDefault: true
+})
+
+
+export function createSchema() {
+  knex.schema.createTableIfNotExists('users', function (table) {
+    table.increments()
+    table.timestamps()
+    table.string('name')
+    table.string('google_id')
+    table.string('google_token')
+  }).return()
+
+
+  knex.schema.createTableIfNotExists('rooms', function (table) {
+    table.increments()
+    table.timestamps()
+    table.string('name')
+    table.text('description')
+    table.integer('user_id')
+
+    table.foreign('user_id').references('Users.id')
+  }).return()
+
+  knex.schema.createTableIfNotExists('questions', function (table) {
+    table.increments()
+    table.timestamps()
+    table.integer('user_id')
+    table.integer('room_id')
+    table.boolean('is_anonymous')
+    table.text('content')
+
+    table.foreign('user_id').references('Users.id')
+    table.foreign('room_id').references('Rooms.id')
+  }).return()
+}
+
+export function createUser({google_id, google_token, name}) {
+  return knex('users')
+    .insert({
+      name,
+      google_id,
+      google_token
+    })
+    .returning('*')
+    .then(ids => ({
+      name, id: ids[0], google_id, google_token
+    }))
+}
+
+export function findUser({id, google_id}) {
+  let query = knex('users')
+    .select('*')
+
+  if (id)
+    return query.where('id', '=', id)
+
+  return query.where('google_id', '=', google_id)
+}
+
+// TODO call this some other way
+createSchema()
+
+
 export default {
   questions: [
     {id: 1, content: 'is this a question?', name: 'anonymous', user_id: '12931283123', room_id: 'venice', timestamp: new Date()},
