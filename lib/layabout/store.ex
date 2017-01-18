5f00a7ebbef011f5832967e5eac707726432a331
Erik Price <github@erikprice.net>
2017-01-18 00:55:30 -0800

    Add get_histogram

--- lib/layabout/store.ex
+++ lib/layabout/store.ex
@@ -12,9 +12,24 @@ defmodule Layabout.Store do
     {:ok, pid}
   end
 
-  def get_user_record(user) do
-    default = %{entries: [[nil, nil]], meta: nil}
-    Agent.get(__MODULE__, &Map.get(&1, user)) || default
+  def get_histogram(user) do
+    hist = 0..23 |> Enum.map(&{&1, 0}) |> Enum.into(%{})
+
+    {_, hist} = get_user_record(user)[:entries]
+    |> Enum.reject(fn {_, e} -> e == nil end)
+    |> Enum.map(fn {b, e} ->
+      hours = Timex.diff(e, b, :hours)
+
+      Enum.into(0..hours, [], fn hr ->
+        Timex.add(b, Timex.Duration.from_hours(hr)).hour
+      end)
+    end)
+    |> List.flatten
+    |> Enum.map_reduce(hist, fn x, acc ->
+      Map.get_and_update(acc, x, &{&1, &1 + 1})
+    end)
+
+    hist
   end
 
   def log_active(user) do
@@ -29,8 +44,8 @@ defmodule Layabout.Store do
     [hd|rest] = record[:entries]
     next =
       case hd do
-        [start, nil] -> [[start, nil]]
-        x            -> [[now, nil], x]
+        {start, nil} -> [{start, nil}]
+        x            -> [{now, nil}, x]
       end
 
     Agent.update(__MODULE__, &Map.put(&1, user,
