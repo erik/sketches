146f4132228a6c47665576571a3a1b770883dd4b
Erik Price <github@erikprice.net>
2017-01-27 16:09:38 -0800

    Clean up store.ex

--- lib/layabout/store.ex
+++ lib/layabout/store.ex
@@ -15,14 +15,14 @@ defmodule Layabout.Store do
   def get_histogram(user) do
     alias Timex.Duration
 
-    get_user_record(user).entries
+    get_user_record(user)
     |> Enum.map(fn {b, e} ->
       span_end = e || DateTime.utc_now
       duration = Timex.diff(span_end, b, :minutes)
 
       for min <- 0..duration do
         time = Timex.add(b, Duration.from_minutes(min))
-        {time.hour, time.minute}
+        {time.hour, 5 * div(time.minute, 5)}
       end
     end)
     |> List.flatten
@@ -34,50 +34,41 @@ defmodule Layabout.Store do
   end
 
   def log_active(user, timestamp \\ DateTime.utc_now) do
-    record = get_user_record(user)
-
-    unless record[:meta] do
-      # TODO: Lookup meta data
-      # TODO: Need next point release of elixir-slack. 0.9.2 is broken on 1.4.0
-    end
-
     entries =
-      case record.entries do
+      case get_user_record(user) do
         # First session seen.
         [] ->
           [{timestamp, nil}]
 
-        # Already have an open session.
-        [{_, e} | _] when is_nil e ->
-          record.entries
+        [{b, e} | rest] when not is_nil(e) ->
+          # Being inactive < 5 minutes just merges into one record
+          if Timex.diff(timestamp, e, :minutes) < 5 do
+            [{b, nil} | rest]
+          else
+            [{timestamp, nil}, {b, e} | rest]
+          end
 
-        [{b, e} | rest] ->
-        # Being inactive < 5 minutes just merges into one record
-        if Timex.diff(timestamp, e, :minutes) < 0 do
-          [{b, nil} | rest]
-        else
-          [{timestamp, nil}, {b, e} | rest]
-        end
+        # Already have an open session.
+        e -> e
       end
 
-    Agent.update(__MODULE__, &Map.put(&1, user,
-          Map.put(record, :entries, entries)))
+    Agent.update(__MODULE__, &Map.put(&1, user, entries))
   end
 
   def log_inactive(user, timestamp \\ DateTime.utc_now) do
-    record = get_user_record(user)
+    entries =
+      case get_user_record(user) do
+        [] ->
+          []
+        [{b, _} | rest] ->
+          [{b, timestamp} | rest]
+      end
 
-    entries = case record.entries do
-                [] -> []
-                [{b, _} | rest] -> [{b, timestamp} | rest]
-              end
+    Agent.update(__MODULE__, &Map.put(&1, user, entries))
 
-    Agent.update(__MODULE__, &Map.put(&1, user,
-          Map.put(record, :entries, entries)))
   end
 
   defp get_user_record(user) do
-    default = %{entries: [], meta: nil}
-    Agent.get(__MODULE__, &Map.get(&1, user)) || default
+    Agent.get(__MODULE__, &Map.get(&1, user)) || []
   end
 end
