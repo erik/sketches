3865934239878c62602130a9ed96cd5d3a36722b
Erik Price <github@erikprice.net>
2017-05-08 19:21:53 -0700

    basic lisp evaluation

--- timetravel.py
+++ timetravel.py
@@ -1,5 +1,7 @@
 CUT_CHARS = {'(', ')', ' '}
 LITERAL_TOKENS = {'(', ')'}
+NODE_TYPES = {'if', 'fn', 'builtin'}
+BUILTINS = {'list', 'print'}
 
 
 def lex(source):
@@ -38,9 +40,13 @@ def parse_exp(tokens):
     if tok == '(':
         return parse_list(rest)
     elif tok == 'number':
-        return value, rest
-    elif tok == 'ident':
-        return value, rest
+        return ('number', value), rest
+    elif tok == 'ident' and value in BUILTINS:
+        return ('builtin', value), rest
+    elif tok == 'ident' and value in NODE_TYPES:
+        return (value, value), rest
+    elif tok == 'ident' and value not in NODE_TYPES:
+        return ('ident', value), rest
     else:
         raise SyntaxError('unexpected token: %s: %s' % (tok, value))
 
@@ -51,7 +57,7 @@ def parse_list(tokens):
     while len(tokens):
         (tok, value) = tokens[0]
         if tok == ')':
-            return lst, tokens[1:]
+            return ('list', lst), tokens[1:]
         elif tok == 'eof':
             raise SyntaxError('unexpected EOF')
         else:
@@ -73,6 +79,69 @@ def parse(source):
         ast.append(value)
 
     return ast
+
+
+class Interpreter(object):
+    def __init__(self, ast):
+        self.commitish = {}
+        self.ast = ast
+
+    def evaluate(self):
+        result = None
+
+        for kind, value in self.ast:
+            print 'eval:', kind, value
+
+            result = self.evaluate_exp((kind, value))
+
+        return result
+
+    def evaluate_exp(self, exp):
+        kind, value = exp
+
+        if kind == 'list':
+            return self.evaluate_list(value)
+        elif kind == 'number':
+            return value
+        elif kind == 'ident':
+            return self.lookup_ident(value)
+        else:
+            # not implemented...
+            raise NotImplemented('oops')
+
+    def evaluate_list(self, lst):
+        if not lst:
+            return
+
+        fn, args = lst[0], lst[1:]
+        return self.evaluate_function(fn, args)
+
+    def evaluate_function(self, fn, args):
+        kind, ident = fn
+
+        if kind == 'builtin':
+            return self.evaluate_builtin(ident, args)
+
+        elif kind == 'if':
+            assert len(args) == 3, '(if cond then else)'
+            cond, then, else_ = args
+
+            if self.evaluate_exp(cond):
+                return self.evaluate_exp(then)
+
+            else:
+                return self.evaluate_exp(else_)
+
+    def evaluate_builtin(self, fn, args):
+        if fn == 'list':
+            return args
+
+        elif fn == 'print':
+            print args
+
+    def lookup_ident(self, ident):
+        if ident in BUILTINS:
+            return ('builtin', ident)
 
 
 if __name__ == '__main__':
