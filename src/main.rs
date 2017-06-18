5e8576b4468a7be27eab4c9d5eb6e3746c6fbbd7
Erik Price <github@erikprice.net>
2017-06-18 13:01:59 -0700

    start sketching out types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+type FreeVariable = u64;
+type VariableName<'a> = &'a str;
+
+/// AST node expression
+enum Expression<'a> {
+    Number(f64),
+    Boolean(bool),
+
+    /// simple variable
+    Variable(&'a VariableName<'a>),
+
+    /// fn (x, y, z) -> expr
+    Lambda(&'a [VariableName<'a>], &'a Expression<'a>),
+
+    /// let x = expr in expr
+    Let(VariableName<'a>, &'a Expression<'a>, &'a Expression<'a>),
+
+    /// some_fn (x, y, z)
+    FnCall(&'a Expression<'a>, &'a[Expression<'a>])
+}
+
+
+enum Type<'a> {
+    Number,
+    Boolean,
+    Unbound(FreeVariable),
+    Lambda(&'a [Type<'a>], &'a Type<'a>),
+
+}
+
+
+type TaggedType<'a> = (Expression<'a>, Type<'a>);
+
+
 fn main() {
     println!("Hello, world!");
 }
