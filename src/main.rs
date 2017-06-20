c1731a351c7f3bead14cf9a3cdb2e5e9baee69a1
Erik Price <github@erikprice.net>
2017-06-19 23:00:38 -0700

    TODO: unify

--- src/main.rs
+++ src/main.rs
@@ -83,6 +83,7 @@ fn w<'a>(expr: &Expression<'a>, env: &mut TypeEnv<'a>, inst: &mut VariableGen) -
         // 1. ensure that the callable in this function call is truly callable.
         // 2. ensure that the number and type of args match
         // 3. return type of body of lambda
+        // TODO: Need unification algo here.
         Expression::FnCall(callable, args) => {
             // FIXME: this seems broken.
             let mut env_ = env.clone();
@@ -118,7 +119,7 @@ fn main() {
     let num = Expression::Number(123.0);
     let incr = Expression::Variable("incr");
     let x = Expression::Variable("x");
-    let args = ["x"];
+    let args = ["x", "y", "z"];
     let args_ = [&num];
     let fn_call = Expression::FnCall(&incr, &args_);
     let lambda = Expression::Lambda(&args, &num);
@@ -127,11 +128,11 @@ fn main() {
     //     "incr", &lambda,
     //     &fn_call);
 
-    let expr = Expression::Let("y", &num, &x);
+    let expr = Expression::Let("x", &num, &x);
 
 
     let mut inst = VariableGen {current: 0};
     let mut env = HashMap::new();
 
-    println!("this is the thing: {:?}\n", w(&expr, &mut env, &mut inst))
+    println!("this is the thing: {:?}\n", w(&lambda, &mut env, &mut inst))
 }
