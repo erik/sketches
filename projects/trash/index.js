2ca97bc88cf38fe0ab7fdf1bcf1857e472d962c5
Erik Price <github@erikprice.net>
2017-09-16 13:11:14 -0700

    simplify code

--- projects/trash/index.js
+++ projects/trash/index.js
@@ -1,107 +1,101 @@
+const toDomNode = (item) => {
+    if (item instanceof Node) {
+        return item;
+    }
+    // Basic thunk support
+    else if (typeof item === 'function') {
+        return toDomNode(item.call(this));
+    }
+
+    return document.createTextNode(item.toString());
+};
+
+
+function createElement (tag, attrs, children) {
+    let el;
+
+    // Special handling for components.
+    if (tag in this.$components) {
+        const component = this.$components[tag];
+        let props = {};
+
+        component.props.forEach(k => {
+            if (!(k in attrs)) {
+                console.warn(`[${tag}]: missing prop: ${k}`);
+                return;
+            }
+
+            props[k] = attrs[k];
+            delete attrs[k];
+        });
+
+        const rendered = component.render.call(props, createElement.bind(this));
+        el = toDomNode(rendered);
+    } else {
+        el = document.createElement(tag);
+    }
+
+    for (const k in attrs) {
+        const v = attrs[k];
+
+        if (typeof v === 'function') {
+            el.addEventListener(k, v.bind(this));
+        } else if (k === 'style') {
+            for (const prop in v) {
+                el.style.setProperty(prop, v[prop]);
+            }
+        } else {
+            el.setAttribute(k, v);
+        }
+    }
+
+    if (!Array.isArray(children)) {
+        children = [children];
+    }
+
+    children.forEach(child => {
+        el.appendChild(toDomNode(child));
+    });
+
+    return el;
+}
+
+
 class Trash {
     constructor (options) {
         this.$data = options.data || {};
-        this.$hooks = options.hooks || {};
-        this.$renderer = options.render;
+        this.$renderFn = options.render;
         this.$components = options.components || {};
-        this.$dirty = true;
+        this.$renderQueued = false;
 
         for (let k in this.$data) {
             Object.defineProperty(this, k, {
                 get: () => this.$data[k],
                 set: (value) => {
-                    this.$notify(k);
+                    this.$queueRender();
                     this.$data[k] = value;
                 }
             });
         }
 
-        if (options.el) {
-            this.$mount(options.el);
-        }
+        options.el && this.$mount(options.el);
     }
 
     $mount (element) {
-        console.log('mounting to el', element);
         this.$el = document.querySelector(element);
-
-        this.$hooks.mounted.call(this);
-        this.$render();
+        this.$queueRender();
     }
 
-    $notify (key) {
-        if (this.$dirty) return;
-        this.$dirty = true;
+    $queueRender () {
+        if (this.$renderQueued) return;
+        this.$renderQueued = true;
 
-        // microtask
-        Promise.resolve().then(() => { this.$render(false); });
-    }
+        // enqueue microtask
+        Promise.resolve().then(() => {
+            const newElem = this.$renderFn(createElement.bind(this));
+            this.$el.replaceChild(newElem, this.$el.firstChild);
 
-    $render (initial) {
-        if (!initial && !this.$dirty) return;
-
-        const domify = (item) => {
-            if (item instanceof Node) { return item; }
-            else if (typeof item === 'function') {
-                return domify(item.call(this));
-            } else if (typeof item === 'string') {
-                return document.createTextNode(item);
-            } else {
-                return document.createTextNode(item.toString());
-            }
-        };
-
-        const createElement = (tag, attrs, children) => {
-            let el;
-
-            // Special handling for components.
-            if (tag in this.$components) {
-                const component = this.$components[tag];
-                let props = {};
-
-                component.props.forEach(k => {
-                    if (!(k in attrs)) {
-                        console.warn(`[${tag}]: missing prop: ${k}`);
-                        return;
-                    }
-
-                    props[k] = attrs[k];
-                    delete attrs[k];
-                });
-
-                const rendered = component.render.call(props, createElement);
-                el = domify(rendered);
-            } else {
-                el = document.createElement(tag);
-            }
-
-            for (const k in attrs) {
-                const v = attrs[k];
-
-                if (typeof v === 'function') {
-                    el.addEventListener(k, v.bind(this));
-                } else if (k === 'style') {
-                    for (const prop in v) {
-                        el.style.setProperty(prop, v[prop]);
-                    }
-                } else {
-                    el.setAttribute(k, v);
-                }
-            }
-
-            if (!Array.isArray(children)) {
-                children = [children];
-            }
-
-            children.forEach(child => {
-                el.appendChild(domify(child));
-            });
-
-            return el;
-        };
-
-        const newElem = this.$renderer(createElement);
-        this.$el.replaceChild(newElem, this.$el.firstChild);
-        this.$dirty = false;
+            this.$renderQueued = false;
+        });
     }
 };
