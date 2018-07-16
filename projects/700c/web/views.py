2ed0cab0e8a81b8347142ca1718f74cbba20c9b9
Erik Price <github@erikprice.net>
2018-07-16 08:51:11 -0700

    requires_login decorator

--- projects/700c/web/views.py
+++ projects/700c/web/views.py
@@ -1,3 +1,5 @@
+import functools
+
 import flask
 
 from web.models import User
@@ -7,11 +9,24 @@ from web.models import User
 html = flask.Blueprint('html', __name__)
 
 
+def requires_login(f):
+    @functools.wraps(f)
+    def decorated(*args, **kwargs):
+        if not flask.g.user:
+            flask.flash('Must be logged in for that!')
+            return flask.redirect(flask.url_for('login'))
+        return f(*args, **kwargs)
+    return decorated
+
 @html.before_request
 def lookup_request_user():
     user = None
+
     if 'uid' in flask.session:
         user = User.by_id(flask.session['uid'])
+
+        if user is None:
+            flask.session.pop('uid')
 
     flask.g.user = user
 
