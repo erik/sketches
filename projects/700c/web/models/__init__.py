ddd30c0ed0f510756f874c807032abf43bd25dd3
Erik Price <github@erikprice.net>
2018-07-18 00:22:55 -0700

    db crud boilerplate

--- projects/700c/web/models/__init__.py
+++ projects/700c/web/models/__init__.py
@@ -1,6 +1,6 @@
 from datetime import datetime as dt
 
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, Model
 from werkzeug.security import generate_password_hash, \
     check_password_hash
 
@@ -8,7 +8,24 @@ from werkzeug.security import generate_password_hash, \
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class CommonMixin(Model):
+    '''Reduce boilerplate'''
+
+    @classmethod
+    def create(cls, **kwargs):
+        inst = cls(**kwargs)
+
+        db.session.add(inst)
+        db.session.commit()
+
+        return inst
+
+    @classmethod
+    def by_id(cls, pk_id):
+        return cls.query.get(pk_id)
+
+
+class User(CommonMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -19,16 +36,10 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, default=dt.utcnow)
     deleted_at = db.Column(db.DateTime())
 
-    @staticmethod
-    def create(name, password):
+    @classmethod
+    def create(cls, name, password):
         hash = generate_password_hash(password)
-
-        user = User(name=name, password_hash=hash)
-
-        db.session.add(user)
-        db.session.commit()
-
-        return user
+        return super().create(name=name, password_hash=hash)
 
     @staticmethod
     def by_name_and_password(name, password):
@@ -38,12 +49,8 @@ class User(db.Model):
                 user.password_hash, password):
             return user
 
-    @staticmethod
-    def by_id(user_id):
-        return User.query.get(user_id)
 
-
-class Trip(db.Model):
+class Trip(CommonMixin, db.Model):
     __tablename__ = 'trips'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -57,14 +64,10 @@ class Trip(db.Model):
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    @staticmethod
-    def create(title, description, user_id):
-        trip = Trip(title=title, description=description, user_id=user_id)
-
-        db.session.add(trip)
-        db.session.commit()
-
-        return trip
+    @classmethod
+    def create(cls, title, description, user_id):
+        return super().create(
+            title=title, description=description, user_id=user_id)
 
     @staticmethod
     def by_user_id(user_id, page=0, per_page=100):
@@ -74,7 +77,7 @@ class Trip(db.Model):
             .pagination(page, per_page, error_out=False)
 
 
-class Status(db.Model):
+class Status(CommonMixin, db.Model):
     __tablename__ = 'statuses'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -89,15 +92,10 @@ class Status(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     trip_id = db.Column(db.Integer, db.ForeignKey('trips.id'))
 
-    @staticmethod
-    def create(title, body, user_id, trip_id):
-        status = Status(title=title, body=body, user_id=user_id,
-                        trip_id=trip_id)
-
-        db.session.add(status)
-        db.session.commit()
-
-        return status
+    @classmethod
+    def create(cls, title, body, user_id, trip_id):
+        return super().create(
+            title=title, body=body, user_id=user_id, trip_id=trip_id)
 
     @staticmethod
     def by_trip_id(trip_id, page=0, per_page=100):
