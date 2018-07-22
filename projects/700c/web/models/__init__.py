3e2c35ded1751456e3a3f659fb1500dfe0ed9cfc
Erik Price <github@erikprice.net>
2018-07-21 23:54:40 -0700

    Start work on including status location

--- projects/700c/web/models/__init__.py
+++ projects/700c/web/models/__init__.py
@@ -1,12 +1,49 @@
+import collections
 from datetime import datetime as dt
+import json
 
 import flask
+import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy, Model
 from werkzeug.security import generate_password_hash, \
     check_password_hash
 
 
 db = SQLAlchemy()
+
+
+# TODO: this doesn't belong in models
+class Location:
+    MAP_URL_TEMPLATE = \
+        'https://maps.googleapis.com/maps/api/staticmap' \
+        '?center={lat},{lng}'\
+        '&zoom=13'\
+        '&size=500x300'\
+        '&sensor=false'\
+        '&markers=color:blue|{lat},{lng}'
+
+    def __init__(self, lat, lng):
+        self.lat = lat
+        self.lng = lng
+
+    def get_map_url(self):
+        return self.MAP_URL_TEMPLATE.format(lat=self.lat, lng=self.lng)
+
+
+class LocationType(sqlalchemy.types.TypeDecorator):
+    impl = sqlalchemy.types.UnicodeText
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps({'lat': value.lat, 'lng': value.lng})
+
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = Location(**json.loads(value))
+
+        return value
 
 
 class CommonMixin(Model):
@@ -104,8 +141,9 @@ class Status(CommonMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
-    title = db.Column(db.String())
-    body = db.Column(db.String())
+    title = db.Column(db.String)
+    body = db.Column(db.String)
+    location = db.Column(LocationType)
 
     created_at = db.Column(db.DateTime, default=dt.utcnow)
     updated_at = db.Column(db.DateTime, default=dt.utcnow)
@@ -115,9 +153,13 @@ class Status(CommonMixin, db.Model):
     trip_id = db.Column(db.Integer, db.ForeignKey('trips.id'))
 
     @classmethod
-    def create(cls, title, body, user_id, trip_id):
+    def create(cls, title, body, user_id, trip_id, location):
         return super().create(
-            title=title, body=body, user_id=user_id, trip_id=trip_id)
+            title=title,
+            body=body,
+            user_id=user_id,
+            trip_id=trip_id,
+            location=location)
 
     @staticmethod
     def by_trip_id(trip_id, page=0, per_page=100):
--- projects/700c/web/views/status.py
+++ projects/700c/web/views/status.py
@@ -1,7 +1,9 @@
+import json
+
 import flask
 from flask import request
 
-from web.models import Status
+from web.models import Location, Status
 from web.views import requires_login, lookup_request_user
 
 
@@ -13,10 +15,16 @@ mod.before_request(lookup_request_user)
 @requires_login
 def create():
     # TODO: real form parsing
+    location = request.form['location']
+    if location != '':
+        location = Location(**json.loads(location))
+    else:
+        location = None
 
     Status.create(
         title=request.form['title'],
         body=request.form['body'],
+        location=location,
         user_id=flask.g.user.id,
         trip_id=0)
 
