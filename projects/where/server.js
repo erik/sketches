73cd794399e9c2c7cd6410a54406ab8d9f697e05
Erik Price <github@erikprice.net>
2018-08-17 00:42:09 -0700

    prettier

--- projects/where/server.js
+++ projects/where/server.js
@@ -14,38 +14,48 @@ const session = require('express-session');
 
 require('dotenv').config();
 
-redis.on('error', (err) => console.error(`redis error: ${err}`));
+redis.on('error', err => console.error(`redis error: ${err}`));
 
 const app = express();
 
 app.use(morgan('common'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.engine('html', expressHandlebars({
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine(
+  'html',
+  expressHandlebars({
     helpers: {
-        withDefault: (a, b) => a || b,
-        humanize: (ts) => moment(ts).fromNow(),
-        json: (val) => new handlebars.SafeString(JSON.stringify(val))
+      withDefault: (a, b) => a || b,
+      humanize: ts => moment(ts).fromNow(),
+      json: val => new handlebars.SafeString(JSON.stringify(val))
     }
-}));
+  })
+);
 app.set('view engine', 'handlebars');
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     saveUninitialized: true,
     resave: true
-}));
+  })
+);
 
-passport.use(new passportGoogle.OAuth2Strategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET_ID,
-    callbackURL: process.env.BASE_URL + '/who/google/callback'
-}, (_1, _2, {emails, id}, done) => {
-    if (emails.some(e => e.value === process.env.GOOGLE_EMAIL)) {
+passport.use(
+  new passportGoogle.OAuth2Strategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET_ID,
+      callbackURL: process.env.BASE_URL + '/who/google/callback'
+    },
+    (_1, _2, { emails, id }, done) => {
+      if (emails.some(e => e.value === process.env.GOOGLE_EMAIL)) {
         return done(null, id);
-    }
+      }
 
-    return done('who are you');
-}));
+      return done('who are you');
+    }
+  )
+);
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
@@ -53,96 +63,97 @@ passport.deserializeUser((user, done) => done(null, user));
 app.use(passport.initialize());
 app.use(passport.session());
 
+function requireAuth(req, res, next) {
+  return next();
+  if (!req.session.loggedIn) {
+    return res.redirect('/who');
+  }
 
-function requireAuth (req, res, next) {
-    return next();
-    if (!req.session.loggedIn) {
-        return res.redirect('/who');
-    }
-
-    return next();
+  return next();
 }
 
-function where () {
-    return new Promise((resolve, reject) => {
-        redis.hgetall('where', (err, data) => {
-            if (err !== null) {
-                console.error('hgetall failed', err);
-                return reject(err);
-            }
+function where() {
+  return new Promise((resolve, reject) => {
+    redis.hgetall('where', (err, data) => {
+      if (err !== null) {
+        console.error('hgetall failed', err);
+        return reject(err);
+      }
 
-            return resolve(
-                Object.values(data || {})
-                    .map(d => JSON.parse(d))
-                    .sort((a, b) => -a.ts.localeCompare(b.ts)));
-        });
+      return resolve(
+        Object.values(data || {})
+          .map(d => JSON.parse(d))
+          .sort((a, b) => -a.ts.localeCompare(b.ts))
+      );
     });
+  });
 }
 
-function here (lat, lng, comment) {
-    const createdAt = new Date();
-    const redisKey = createdAt.toISOString();
+function here(lat, lng, comment) {
+  const createdAt = new Date();
+  const redisKey = createdAt.toISOString();
 
-    const point = JSON.stringify({
-        lat,
-        lng,
-        comment: escapeHtml(comment),
-        ts: createdAt,
-        key: redisKey
+  const point = JSON.stringify({
+    lat,
+    lng,
+    comment: escapeHtml(comment),
+    ts: createdAt,
+    key: redisKey
+  });
+
+  return new Promise((resolve, reject) => {
+    redis.hset('where', redisKey, point, err => {
+      if (err !== null) {
+        console.error('hset failed', err);
+        return reject(err);
+      }
+
+      return resolve(null);
     });
-
-
-    return new Promise((resolve, reject) => {
-        redis.hset('where', redisKey, point, (err) => {
-            if (err !== null) {
-                console.error('hset failed', err);
-                return reject(err);
-            }
-
-            return resolve(null);
-        });
-    });
+  });
 }
 
 app.get('/', (req, res) => {
-    where()
-        .then((points) => {
-            res.render('where.html', { who: process.env.WHO, points });
-        })
-        .catch(() => res.sendStatus(500));
+  where()
+    .then(points => {
+      res.render('where.html', { who: process.env.WHO, points });
+    })
+    .catch(() => res.sendStatus(500));
 });
 
 app.get('/who', passport.authenticate('google', { scope: ['email'] }));
 
-app.get('/who/google/callback',
-        passport.authenticate('google', { failureRedirect: '/who' }),
-        (req, res) => {
-            req.session.loggedIn = true;
-            res.redirect('/here');
-        });
+app.get(
+  '/who/google/callback',
+  passport.authenticate('google', { failureRedirect: '/who' }),
+  (req, res) => {
+    req.session.loggedIn = true;
+    res.redirect('/here');
+  }
+);
 app.get('/here', requireAuth, (req, res) => {
-    where()
-        .then((points) => res.render('here.html', { points }))
-        .catch(() => res.sendStatus(500));
+  where()
+    .then(points => res.render('here.html', { points }))
+    .catch(() => res.sendStatus(500));
 });
 
 app.post('/here', requireAuth, (req, res) => {
-    here(req.body.lat, req.body.lng, req.body.comment)
-        .then(() => res.redirect('/'))
-        .catch(() => res.sendStatus(500));
+  here(req.body.lat, req.body.lng, req.body.comment)
+    .then(() => res.redirect('/'))
+    .catch(() => res.sendStatus(500));
 });
 
 app.post('/here/:id/delete', requireAuth, (req, res) => {
-    if (!req.params.id) return res.sendStatus(400);
+  if (!req.params.id) return res.sendStatus(400);
 
-    redis.hdel('where', req.params.id, (err) => {
-        if (err !== null) {
-            console.error('hdel failed', err);
-            return res.sendStatus(500);
-        }
+  redis.hdel('where', req.params.id, err => {
+    if (err !== null) {
+      console.error('hdel failed', err);
+      return res.sendStatus(500);
+    }
 
-        return res.redirect('/here');
-    });
+    return res.redirect('/here');
+  });
 });
 
 app.listen(process.env.PORT);
