4de74e635977e08a769108cb8c9ada4969510b95
Erik Price <github@erikprice.net>
2018-11-03 23:28:38 -0700

    Initial commit

---
+++ projects/typefaceless/addon/background.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+const CAPTURED_URLS = [
+  'https://fonts.googleapis.com/*',
+  'http://fonts.googleapis.com/*',
+  'https://fonts.gstatic.com/*',
+  'http://fonts.gstatic.com/*',
+];
+
+// TODO: Pick something more general.
+const FACELESS_UA = 'typefaceless';
+const FACELESS_REFERRER = 'https://google.com';
+
+const HEADER_REWRITE_RULES = {
+  'user-agent': FACELESS_UA,
+  'referer': FACELESS_REFERRER,
+};
+
+function rewriteHeaders(e) {
+  for (let header of e.requestHeaders) {
+    let value = HEADER_REWRITE_RULES[header.name.toLowerCase()];
+    if (typeof value !== 'undefined') {
+      header.value = value;
+    }
+  }
+
+  return {
+    requestHeaders: e.requestHeaders
+  };
+}
+
+// backcompat for chrome
+if (typeof browser === 'undefined') {
+  browser = chrome;
+}
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+  rewriteHeaders,
+  {urls: CAPTURED_URLS},
+  ['blocking', 'requestHeaders']);
