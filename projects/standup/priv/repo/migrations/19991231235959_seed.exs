452db0b0804d571b49c5f935756409966a384174
Erik Price <github@erikprice.net>
2019-01-26 11:35:04 -0800

    Merge seed migrations into one file

---
+++ projects/standup/priv/repo/migrations/19991231235959_seed.exs
@@ -0,0 +1,76 @@
+defmodule Standup.Repo.Migrations.Seed do
+  use Ecto.Migration
+
+  def change do
+
+    # Users
+    create table(:users) do
+      add :email, :string
+      add :email_verified_at, :utc_datetime
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+
+    # Authors
+    create table(:authors) do
+      add :name, :text
+      add :image_url, :text
+      add :bio, :text
+      add :location, :text
+
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create unique_index(:authors, [:user_id])
+
+    # Journals
+    create table(:journals) do
+      add :title, :text
+      add :description, :text
+      add :tags, {:array, :text}
+      add :public, :boolean, default: true, null: false
+      add :started_at, :utc_datetime
+      add :completed_at, :utc_datetime
+
+      add :author_id, references(:authors, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:journals, [:author_id])
+
+    # Entries
+    create table(:entries) do
+      add :title, :text
+      add :body, :text
+      add :public, :boolean, default: true, null: false
+
+      add :author_id, references(:authors, on_delete: :nothing)
+      add :journal_id, references(:journals, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:entries, [:author_id])
+    create index(:entries, [:journal_id])
+
+    # Replies
+    create table(:replies) do
+      add :body, :text
+
+      add :user_id, references(:users, on_delete: :nothing)
+      add :entry_id, references(:entries, on_delete: :nothing)
+      add :parent_id, references(:replies, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:replies, [:user_id])
+    create index(:replies, [:entry_id])
+    create index(:replies, [:parent_id])
+  end
+end
