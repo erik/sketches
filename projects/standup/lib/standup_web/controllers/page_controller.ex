6b2cc3a89062347530f2683d18674c7a295be1f5
Erik Price <github@erikprice.net>
2019-01-29 09:03:56 -0800

    Throw a TODO list on index

--- projects/standup/lib/standup_web/controllers/page_controller.ex
+++ projects/standup/lib/standup_web/controllers/page_controller.ex
@@ -1,7 +1,17 @@
 defmodule StandupWeb.PageController do
   use StandupWeb, :controller
 
+  @todo [
+    "new user experience (create author record)",
+    "create journal",
+    "create entry",
+    "index listing page",
+    "user detail page",
+    "hook up mailer for login (SMTP/mailgun?)",
+    "support oauth providers?",
+  ]
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", todo: @todo)
   end
 end
