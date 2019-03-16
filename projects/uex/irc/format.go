9fc2bef56c0818942472b2b87bd75210d39e9c1f
Erik Price <github@erikprice.net>
2019-03-15 23:26:11 -0700

    Add support for colors and styling

--- projects/uex/irc/format.go
+++ projects/uex/irc/format.go
@@ -3,6 +3,7 @@ package irc
 import (
 	"fmt"
 	"hash/fnv"
+	"regexp"
 	"time"
 
 	"gopkg.in/sorcix/irc.v2"
@@ -16,7 +17,7 @@ const (
 
 // TODO: Maybe use 256 color mode? \x1b[38;5;NUMBERm
 var nickColors = []string{
-	"\x1B[30m", "\x1B[30;1m", "\x1B[30;2m",
+	"", "\x1B[30;1m", "\x1B[30;2m",
 	"\x1B[31m", "\x1B[31;1m", "\x1B[31;2m",
 	"\x1B[32m", "\x1B[32;1m", "\x1B[32;2m",
 	"\x1B[33m", "\x1B[33;1m", "\x1B[33;2m",
@@ -36,6 +37,67 @@ func colorizeNick(nick string) string {
 	return fmt.Sprintf("%s%15s%s", col, nick, resetColors)
 }
 
+// https://github.com/myano/jenni/wiki/IRC-String-Formatting
+// https://misc.flogisoft.com/bash/tip_colors_and_formatting
+var ircToTerm = map[string]string{
+	"00": "37",   // white
+	"01": "30",   // black
+	"02": "34",   // blue (navy)
+	"03": "32",   // green
+	"04": "31",   // red
+	"05": "35",   // brown (maroon)
+	"06": "35;1", // purple
+	"07": "33",   // orange (olive)
+	"08": "33;1", // yellow
+	"09": "32;1", // light green (lime)
+	"10": "34;1", // teal (a green/blue cyan)
+	"11": "34;1", // light cyan (cyan / aqua)
+	"12": "34;1", // light blue (royal)
+	"13": "37;1", // pink (light purple / fuchsia)
+	"14": "37;1", // grey
+	"15": "37;1", // light grey (silver)
+}
+
+// stylizeLine converts IRC formatting codes to ANSI terminal codes.
+func stylizeLine(line string) string {
+	r := regexp.MustCompile("[\x02\x1D\x0F\x1F]|(\x03(?:\\d\\d(?:,\\d\\d)?)?)")
+	line = r.ReplaceAllStringFunc(line, func(m string) string {
+		code := "0"
+		switch m[0] {
+		// Bold
+		case '\x02':
+			code = "1"
+
+		// Colored text
+		case '\x03':
+			// Ignore background codes
+			if len(m) >= 3 {
+				code, _ = ircToTerm[m[1:3]]
+			}
+
+		// Italic text
+		case '\x1D':
+			code = "4"
+
+		// Underlined text
+		case '\x1F':
+			code = "4"
+
+		// Swap background and foreground colors ("reverse video")
+		case '\x16':
+			code = "7"
+
+		// reset all formatting
+		case '\x0F':
+			code = "0"
+		}
+
+		return fmt.Sprintf("\x1B[%sm", code)
+	})
+
+	return line + "\x1B[0m"
+}
+
 func formatMessage(m *irc.Message) string {
 	ts := time.Now().Format(timestampFormat)
 	sender := alertSender
@@ -44,7 +106,7 @@ func formatMessage(m *irc.Message) string {
 	switch m.Command {
 	case irc.PRIVMSG, irc.NOTICE:
 		sender = m.Prefix.Name
-		line = m.Trailing()
+		line = stylizeLine(m.Trailing())
 
 	case irc.RPL_TOPIC:
 		sender = m.Prefix.Name
