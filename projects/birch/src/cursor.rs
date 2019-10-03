d353fb22e8a839a4f75841d0996d9f8bd8661d4c
Erik Price <github@erikprice.net>
2019-10-03 15:00:17 -0700

    Sketch out a bit more of Cursor

--- projects/birch/src/cursor.rs
+++ projects/birch/src/cursor.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code)]
 
 use std::collections::HashMap;
+use std::path::Path;
+
+use crate::proto::RawMessage;
 
 /// Save positions of individual clients through the buffer history.
 ///
@@ -30,13 +33,36 @@ type CursorState = HashMap<CursorKey, CursorPosition>;
 /// Conceptual representation of a buffer. Internally can paginate
 /// over multiple chunks.
 struct Buffer {
-    network: String
+    network: String,
 }
 
+/// Date-partitioning of buffer.
 // TODO: actual read/write ops to file
 struct BufferChunk {}
 
 /// Keep track of where each client is in each buffer
+// TODO: In memory buffer of recent log lines? Probably overkill.
 struct BufferManager {
     state: CursorState,
+    buffer_dir: Path,
+}
+
+impl BufferManager {}
+
+struct BufferIter {
+    pos: CursorPosition,
+}
+
+impl Iterator for BufferIter {
+    type Item = RawMessage;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unimplemented!()
+    }
+}
+
+impl DoubleEndedIterator for BufferIter {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        unimplemented!()
+    }
 }
