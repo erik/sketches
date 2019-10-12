1c1ce67620c625c05a043af77f68d3dceadb4663
Erik Price <github@erikprice.net>
2019-10-11 23:09:28 -0700

    Move IrcSocket logic to socket.rs

--- projects/birch/src/socket.rs
+++ projects/birch/src/socket.rs
@@ -1,6 +1,9 @@
 use std::io::prelude::*;
-use std::io::{BufRead, Error, ErrorKind, Result};
+use std::io::{BufRead, BufReader, Error, ErrorKind, Result};
+use std::net::TcpStream;
+use std::time::Duration;
 
+use crate::network::NetworkConnection;
 use crate::proto::RawMessage;
 
 pub trait IrcWriter {
@@ -35,5 +38,88 @@ impl<T: BufRead> IrcReader for T {
         RawMessage::parse(line).ok_or_else(|| {
             Error::new(ErrorKind::Other, "failed to parse message from line")
         })
+    }
+}
+
+pub struct IrcSocketConfig<'a> {
+    pub nick: &'a str,
+    pub addr: &'a str,
+    pub max_retries: Option<usize>,
+}
+
+pub struct IrcSocket<'a> {
+    config: IrcSocketConfig<'a>,
+}
+
+impl<'a> IrcSocket<'a> {
+    pub fn new(config: IrcSocketConfig<'a>) -> Self {
+        Self { config }
+    }
+
+    // TODO: this is kind of gross, clean this up
+    fn create_stream(&self) -> Result<TcpStream> {
+        let _create_stream = || {
+            let stream = TcpStream::connect(self.config.addr)?;
+            stream.set_read_timeout(Some(Duration::from_secs(180)))?;
+            Ok(stream)
+        };
+
+        let max_retries = self.config.max_retries;
+
+        let mut i = 0;
+        loop {
+            let stream = _create_stream();
+            let over_max_tries = max_retries.map(|max| i > max).unwrap_or(false);
+
+            if stream.is_ok() || over_max_tries {
+                return stream;
+            }
+
+            i += 1
+        }
+    }
+
+    /// On connection close, return `true` when the connection should
+    /// be restarted, and false otherwise. If there is a
+    /// non-recoverable exception, return the error.
+    ///
+    /// TODO: Need to come up with the clean exit concept.
+    fn connect(&self) -> Result<bool> {
+        let mut stream = self.create_stream()?;
+        let mut reader = BufReader::new(stream.try_clone()?);
+        let mut network = NetworkConnection::new(self.config.nick, &mut stream);
+
+        if network.initialize().is_err() {
+            return Ok(true);
+        }
+
+        loop {
+            let result = reader.read_message().and_then(|msg| {
+                println!("[birch <- \u{1b}[37;1mnet\u{1b}[0m] {}", msg);
+                network.handle(&msg)
+            });
+
+            if result.is_err() {
+                println!("read failed");
+                break;
+            }
+        }
+
+        Ok(true)
+    }
+
+    pub fn start_loop(&mut self) -> Result<()> {
+        loop {
+            match self.connect() {
+                Ok(true) => println!("connection terminated, restarting"),
+                Ok(false) => break,
+                Err(err) => {
+                    println!("connection failed: {:?}", err);
+                    return Err(err);
+                }
+            }
+        }
+
+        Ok(())
     }
 }
