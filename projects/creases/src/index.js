cadc06b18b1bb486d29c5a24f7e5e30aee765213
Erik Price <github@erikprice.net>
2019-12-21 01:13:10 -0800

    Parse TCX courses into normalized format

--- projects/creases/src/index.js
+++ projects/creases/src/index.js
@@ -4,10 +4,21 @@ import React, { createElement as h, useState, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 import { useDropzone } from 'react-dropzone';
 
-const Dropzone = () => {
-  const onDrop = useCallback(files => {
-    // TODO: handle these files;
-    console.log('Received dropped files', files);
+import parser from './parser.js';
+
+
+const Dropzone = (setScreen) => () => {
+  const onDrop = useCallback((files) => {
+    // Only take the first file uploaded;
+    const file = files[0];
+
+    parser.parseFile(file)
+      .then(it => console.log('parsed to:', it))
+      .then(() => setScreen(AppScreens.RENDER_PDF))
+      .catch(err => {
+        console.error(err);
+        window.alert("Something went wrong, maybe something's wrong with that file?");
+      });
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
@@ -22,10 +33,10 @@ const Dropzone = () => {
   ]);
 };
 
-const DropScreen = () => h('div', { className: 'text-center'}, [
+const DropScreen = (setScreen) => h('div', { className: 'text-center'}, [
   h('h1', null, 'Drop a route here to begin'),
-  h(Dropzone, null, []),
-  h('p', null, 'Supported file types: TCX'),
+  h(Dropzone(setScreen), null, []),
+  h('p', null, 'Supported file types: FIT'),
 ]);
 
 const ErrorScreen = (details) => h('div', {}, [
@@ -33,15 +44,24 @@ const ErrorScreen = (details) => h('div', {}, [
   h('p', null, details || 'an unexpected error occurred')
 ]);
 
+const AppScreens = {
+  DROP: 'drop',
+  RENDER_OPTIONS: 'render_options',
+  RENDER_PDF: 'render_pdf',
+  ERROR: 'error'
+};
+
 const App = () => {
-  const [screen, setScreen] = useState('drop');
+  const [screen, setScreen] = useState(AppScreens.DROP);
 
   switch (screen) {
-  case 'drop': return DropScreen();
-
-  case 'options':
-  default:
+  case AppScreens.DROP: return DropScreen(setScreen);
+  case AppScreens.RENDER_OPTIONS:
+  case AppScreens.RENDER_PDF:
     return ErrorScreen('This is not implemented yet!');
+
+  default:
+    return ErrorScreen(`A bug! Unknown screen type: ${screen}`);
   }
 };
 
---
+++ projects/creases/src/parser.js
@@ -0,0 +1,122 @@
+"use strict";
+
+import xml2js from 'xml2js';
+
+
+// Routes look like this:
+//
+// All measurements are in meters.
+// { name: String
+// , totalDistance: Double
+// , wayPoints: [
+//   { kind: String
+//   . name: String
+//   . description: String
+//   , latitude: Double
+//   , longitude: Double
+//   }]
+// , routePoints: [
+//   { latitude: Double
+//   , longitude: Double
+//   , altitude: Double
+//   , cumulativeDistance: Double
+//   }]
+// }
+
+
+const readFileContents = (file, isBinary) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const result = e.target.result;
+      return resolve(result);
+    };
+
+    if (isBinary) {
+      reader.readAsArrayBuffer(file);
+    } else {
+      reader.readAsText(file);
+    }
+  });
+};
+
+function extractTCXRoute(tcx) {
+  if (!tcx.TrainingCenterDatabase) {
+    throw new Error('Unexpected formatting for TCX file');
+  }
+  tcx = tcx.TrainingCenterDatabase;
+
+  if (!tcx.Courses || tcx.Courses.length === 0) {
+    throw new Error('No courses defined, is this an activity instead of a route?');
+  }
+
+  // Get deeply nested key structure, safely.
+  const get = (root, k) => {
+    let n = root;
+    for (const part of k.split('.')) {
+      n = n[part];
+      if (typeof n === 'undefined') break;
+    }
+    return n;
+  };
+
+  const course = get(tcx, 'Courses.0.Course.0');
+
+  const route = {
+    name: get(course, 'Name.0') || 'Untitled Route',
+    totalDistance: 0.0,
+    routePoints: [],
+    wayPoints: []
+  };
+
+  for (let pt of get(course, 'Track.0.Trackpoint')) {
+    route.totalDistance = +get(pt, 'DistanceMeters.0');
+    route.routePoints.push({
+      altitude: +get(pt, 'AltitudeMeters.0') || 0.0,
+      latitude: +get(pt, 'Position.0.LatitudeDegrees.0'),
+      longitude: +get(pt, 'Position.0.LongitudeDegrees.0'),
+      cumulativeDistance: route.totalDistance
+    });
+  }
+
+  for (let pt of course.CoursePoint) {
+    route.wayPoints.push({
+      name: get(pt, 'Name.0') || 'Untitled Point',
+      description: get(pt, 'Notes.0') || '',
+      latitude: +get(pt, 'Position.0.LatitudeDegrees.0'),
+      longitude: +get(pt, 'Position.0.LongitudeDegrees.0'),
+    });
+  }
+
+  return route;
+}
+
+async function parseTCX(file) {
+  const contents = await readFileContents(file, false);
+  const xmlParser = new xml2js.Parser();
+
+  return new Promise((resolve, reject) => {
+    xmlParser.parseString(contents, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log('TCX object is thius', result)
+        resolve(extractTCXRoute(result));
+      }
+    });
+  });
+}
+
+export async function parseFile(file) {
+  const extension = file.name.split('.').pop();
+  switch ((extension || '').toUpperCase()) {
+  case 'TCX':
+    return await parseTCX(file);
+
+  default:
+    throw new Error(`Unsupported file type: ${extension}`);
+  }
+}
+
+
+export default { parseFile };
