821290ba939953ed7af07265697b0349b894a5c7
Erik Price <github@erikprice.net>
2020-01-18 23:36:55 -0800

    Add conditional highlighting for selected route.

--- projects/tpoafptarbmit/src/index.ts
+++ projects/tpoafptarbmit/src/index.ts
@@ -1,9 +1,10 @@
 import * as L from 'leaflet';
+import * as GeoJson from 'geojson';
 
 
 const CALIFORNIA_DONUTS = {lat: 34.0688093, lng: -118.2930864};
 
-const LAYERS = {
+const LAYERS: {[key: string]: L.TileLayer} = {
     OSM: L.tileLayer(
         'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         {
@@ -25,24 +26,36 @@ const LAYERS = {
             maxZoom: 17,
             attribution:
             'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-        }
-    )
+        }),
+    'Stamen': L.tileLayer(
+        'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png', {
+            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            subdomains: 'abcd',
+            minZoom: 0,
+            maxZoom: 20,
+        })
 };
 
-function fetchRoutes() {
+function fetchRoutes(): Promise<GeoJson.FeatureCollection> {
     return fetch('/data/index.geojson')
-        .then(res => res.json());
+        .then(res => res.json())
+        .then(json => <GeoJson.FeatureCollection>json);
 }
 
 (function() {
+    let geoJson: L.GeoJSON;
+
     const donutShop = L.marker(CALIFORNIA_DONUTS, {
-        icon: L.divIcon({ html: `<span class="text-3xl">🍩</span` })
+        icon: L.divIcon({
+            className: 'donut-shop-icon',
+            html: `<span class="text-3xl">🍩</span`
+        })
     });
 
     const map = L.map('map', {
         center: CALIFORNIA_DONUTS,
-        zoom: 14,
-        layers: [LAYERS.OSM, donutShop],
+        zoom: 13,
+        layers: [LAYERS['Stamen'], donutShop],
     });
 
     L.control
@@ -52,15 +65,46 @@ function fetchRoutes() {
         })
         .addTo(map);
 
-    fetchRoutes()
-        .then(routes => L.geoJSON(routes, {
-            onEachFeature: (feature, layer) => {
-                const p = feature.properties;
-                layer.bindPopup(`
+    // TODO: Link out to geojson.io for full resolution map.
+    const popupForFeature = (props: GeoJson.GeoJsonProperties) => `
 <div class="route-info">
-    <h1 class="text-xl font-bold">#${p.number} - ${p.name}</h1>
-    <div class="text-sm">${p.description}</div>
-`);
-            }
-        }).addTo(map));
+<h1 class="text-xl font-bold">#${props?.number ?? ''} - ${props?.name ?? 'Unnamed'}</h1>
+<div class="text-sm">${props?.description ?? 'No description.'}</div>
+</div>`;
+
+    const routeStyles: {[key: string]: L.PathOptions} = {
+        base: {
+            color: '#A0C8D8',
+            weight: 2,
+        },
+        background: {
+            color: '#A0C8D83F',
+            weight: 2,
+        },
+        highlight: {
+            color: '#B61326',
+            weight: 3,
+        },
+    };
+
+    // Last route we had highlighted, so that it can be cleared.
+    let previousRoute: L.FeatureGroup | null;
+
+    const createGeoJsonLayer = (routes: GeoJson.FeatureCollection) => L.geoJSON(routes, {
+        style: routeStyles.base,
+        onEachFeature: (feature, layer: L.FeatureGroup) => {
+            layer.bindPopup(popupForFeature(feature.properties));
+            layer.on('click', () => {
+                previousRoute?.setStyle(routeStyles.base);
+                previousRoute = layer;
+
+                layer.bringToFront();
+                layer.setStyle(routeStyles.highlight);
+            });
+        }
+    });
+
+    fetchRoutes().then(routes => {
+        geoJson = createGeoJsonLayer(routes).addTo(map);
+    });
 })();
