e499f9f3962794454dc95d716a68b15397318ba1
Erik Price <github@erikprice.net>
2020-01-29 23:40:54 -0800

    Enforce non-NULL values in base SQL schema.

--- projects/birch/src/sql/schema/base.sql
+++ projects/birch/src/sql/schema/base.sql
@@ -2,17 +2,17 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS networks (
     name        TEXT PRIMARY KEY
-  , address     TEXT               -- `host:port`
-  , nick        TEXT               -- Initial nick
-  , server_pass TEXT               -- Credentials to send in PASS command on startup
-  , client_pass TEXT               -- Encoding `user:crypt(password)` for client logins
+  , address     TEXT NOT NULL     -- `host:port`
+  , nick        TEXT NOT NULL     -- Initial nick
+  , server_pass TEXT              -- Credentials to send in PASS command on startup
+  , client_pass TEXT NOT NULL     -- Encoding `user:crypt(password)` for client logins
 );
 
 CREATE TABLE IF NOT EXISTS cursors (
-    id            TEXT
-  , network_name  TEXT
-  , channel       TEXT
-  , position      INTEGER
+    id            TEXT    NOT NULL
+  , network_name  TEXT    NOT NULL
+  , channel       TEXT    NOT NULL
+  , position      INTEGER NOT NULL
 
   , UNIQUE(id, network_name, channel)
   , FOREIGN KEY (network_name) REFERENCES networks (name)
@@ -20,10 +20,10 @@ CREATE TABLE IF NOT EXISTS cursors (
 
 CREATE TABLE IF NOT EXISTS logs (
     id            INTEGER PRIMARY KEY AUTOINCREMENT
-  , network_name  TEXT
-  , timestamp     INTEGER
-  , channel       TEXT
-  , raw_line      TEXT
+  , network_name  TEXT    NOT NULL
+  , timestamp     INTEGER NOT NULL
+  , channel       TEXT    NOT NULL
+  , raw_line      TEXT    NOT NULL
 
   , FOREIGN KEY (network_name) REFERENCES networks (name)
 );
