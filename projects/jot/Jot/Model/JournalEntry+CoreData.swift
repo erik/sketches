a18b6da80e842dce12ede5a226abfa49863f426a
Erik Price <github@erikprice.net>
2022-01-15 23:43:40 +0100

    Add missing extension file

---
+++ projects/jot/Jot/Model/JournalEntry+CoreData.swift
@@ -0,0 +1,50 @@
+import CoreData
+import Foundation
+
+public extension JournalEntry {
+    static func getOrCreateFor(date: Date, using: NSManagedObjectContext) -> JournalEntry {
+        let fetchRequest = JournalEntry.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "%K == %@", #keyPath(JournalEntry.date), date as NSDate)
+
+        do {
+            if let journal = (try using.fetch(fetchRequest) as [JournalEntry]).first {
+                return journal
+            }
+        } catch {
+            print("failed to fetch JournalEntry \(error)")
+        }
+
+        let journal = JournalEntry(context: using)
+        journal.date = Calendar.current.startOfDay(for: Date())
+        using.insert(journal)
+
+        return journal
+    }
+
+    func addTodo(of task: String, using moc: NSManagedObjectContext) {
+        let todoItem = TodoItem(context: moc)
+
+        moc.performAndWait {
+            todoItem.journalEntry = self
+            todoItem.createdAt = Date()
+            todoItem.isRemoved = false
+            todoItem.isCompleted = false
+            todoItem.task = task
+
+            self.addToTodoItems(todoItem)
+
+            moc.insert(todoItem)
+            try! moc.save()
+        }
+    }
+
+    var noteOrEmpty: String {
+        get { note ?? "" }
+        set { print("setting = \(newValue)"); note = newValue }
+    }
+
+    var todoItemsArray: [TodoItem] {
+        let set = (todoItems?.set ?? []) as! Set<TodoItem>
+        return set.sorted { $0.createdAt! < $1.createdAt! }
+    }
+}
