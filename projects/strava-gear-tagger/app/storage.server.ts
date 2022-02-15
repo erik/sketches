43fd92961ee535b5756b353b46df2b71cdb46b0c
Erik Price <github@erikprice.net>
2022-02-15 19:36:24 +0100

    Fix token refresh logic

--- projects/strava-gear-tagger/app/storage.server.ts
+++ projects/strava-gear-tagger/app/storage.server.ts
@@ -1,14 +1,19 @@
+import { json } from "remix";
 import * as strava from "~/util/strava";
 
 
 export async function getStravaCredentials(athleteId: string) {
     let token = await KV_STRAVA_OAUTH.get(`${athleteId}`);
-    console.log('here is what', token)
-    token = JSON.parse(token || '{}');
+    console.log('Token for athleteId=', athleteId, 'token=', token);
 
-    if (!token.expiresAt || new Date(token.expiresAt) < new Date()) {
-        console.log('Token appears expired:', token);
-        token = strava.refreshToken(token);
+    if (token == null) {
+        throw json({ msg: "unauthorized" }, { status: 401 });
+    }
+
+    token = JSON.parse(token);
+
+    if (new Date(token.expiresAt) < new Date()) {
+        token = await strava.refreshToken(token);
         await setStravaCredentials(athleteId, token);
     }
 
