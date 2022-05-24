2885ecede0850d180b66b3c982f95823df0c2754
Erik Price <github@erikprice.net>
2022-05-24 22:44:19 +0200

    Wire up A*

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use osmpbfreader::{
     objects::{NodeId as OsmNodeId, WayId as OsmWayId},
     OsmObj, OsmPbfReader, Tags,
 };
-use petgraph::csr::Csr;
+use petgraph::{algo::astar, csr::Csr, visit::EdgeRef};
 
-type WayId = u32;
 type NodeId = u32;
-type EdgeId = u32;
+type EdgeId = (NodeId, NodeId);
 
 #[derive(Debug, Copy, Clone)]
 struct Coordinate {
@@ -35,10 +34,11 @@ struct EdgeData {
     geometry: Vec<Coordinate>,
 }
 
-struct Graph {
+struct OsmGraph {
     csr: Csr<NodeId, ()>,
     // TODO: Can be vec
     node_data: HashMap<NodeId, NodeData>,
+    edge_data: HashMap<EdgeId, EdgeData>,
 }
 
 impl Coordinate {
@@ -175,12 +175,11 @@ fn strip_tags(tags: &Tags) -> Option<Tags> {
 
 // TODO: we can avoid hashmaps and the ID assignment counters by using a bitmap
 // TODO: Attach metadata to each edge
-fn construct_graph() -> Result<Graph, std::io::Error> {
+fn construct_graph() -> Result<OsmGraph, std::io::Error> {
     let f = std::fs::File::open("./data/andorra.osm.pbf").unwrap();
     let mut pbf = OsmPbfReader::new(f);
 
     let node_id_mapping = construct_node_id_mapping(&mut pbf);
-    let mut seen_way = false;
 
     // Reset to start of file for next iteration.
     pbf.rewind().unwrap();
@@ -188,74 +187,91 @@ fn construct_graph() -> Result<Graph, std::io::Error> {
     // TODO: Can be a Vec<> using NodeId as the index.
     let mut node_geom = HashMap::<OsmNodeId, Coordinate>::with_capacity(node_id_mapping.len());
     let mut node_data = HashMap::<NodeId, NodeData>::new();
-    let mut edges = HashMap::<(NodeId, NodeId), EdgeData>::new();
 
+    let mut edge_data = HashMap::<(NodeId, NodeId), EdgeData>::new();
+
+    let mut seen_way = false;
     for obj in pbf.par_iter() {
         let obj = obj.unwrap();
-        if let OsmObj::Node(ref node) = obj {
-            assert!(seen_way == false, "input files MUST be sorted!");
+        match obj {
+            OsmObj::Relation(_) => {}
 
-            if let Some(node_id) = node_id_mapping.get(&node.id) {
-                let coord = Coordinate {
-                    lat: node.lat() as f32,
-                    lon: node.lon() as f32,
-                };
+            OsmObj::Node(ref osm_node) => {
+                assert!(seen_way == false, "input files MUST be sorted!");
 
-                // TODO: Populate node data as well
-                node_geom.insert(node.id, coord);
-            }
-        }
+                if let Some(node) = node_id_mapping.get(&osm_node.id) {
+                    let coord = Coordinate {
+                        lat: osm_node.lat() as f32,
+                        lon: osm_node.lon() as f32,
+                    };
 
-        if let OsmObj::Way(ref way) = obj {
-            seen_way = true;
-            if way.nodes.len() < 2 || !is_way_routeable(&way.tags) {
-                continue;
-            }
+                    // TODO: Remove deduplication
+                    node_geom.insert(osm_node.id, coord);
 
-            let mut accumulated_dist = 0_u32;
-            let mut prev_coord: Option<Coordinate> = None;
-            let mut prev_node_id: Option<NodeId> = None;
-
-            for osm_node_id in way.nodes.iter() {
-                let node = node_id_mapping
-                    .get(osm_node_id)
-                    .expect("Missing node info from way");
-
-                let coord = node_geom.get(osm_node_id).expect("missing node geometry");
-                if let Some(prev_coord) = prev_coord {
-                    accumulated_dist += prev_coord.dist_to(coord);
+                    if let FirstPassNode::Routing(id) = node {
+                        node_data.insert(
+                            *id,
+                            NodeData {
+                                coord,
+                                tags: strip_tags(&osm_node.tags),
+                            },
+                        );
+                    }
                 }
-                prev_coord = Some(*coord);
+            }
 
-                if let FirstPassNode::Routing(node_id) = node {
-                    if let Some(prev_id) = prev_node_id {
-                        let key = (prev_id, *node_id);
-                        // TODO: Duplicate edges are possible! How do we handle this in CSR?
-                        //   https://www.openstreetmap.org/way/1060404609
-                        //   https://www.openstreetmap.org/way/1060404608
-                        //
-                        // For now, just go with whatever we see first...
-                        if !edges.contains_key(&key) {
-                            // Skip...
-                            edges.insert(
+            OsmObj::Way(ref way) => {
+                seen_way = true;
+                if way.nodes.len() < 2 || !is_way_routeable(&way.tags) {
+                    continue;
+                }
+
+                let mut accumulated_dist = 0_u32;
+                let mut prev_coord: Option<Coordinate> = None;
+                let mut prev_node_id: Option<NodeId> = None;
+
+                for osm_node_id in way.nodes.iter() {
+                    let node = node_id_mapping
+                        .get(osm_node_id)
+                        .expect("Missing node info from way");
+
+                    let coord = node_geom.get(osm_node_id).expect("missing node geometry");
+                    if let Some(prev_coord) = prev_coord {
+                        accumulated_dist += prev_coord.dist_to(coord);
+                    }
+                    prev_coord = Some(*coord);
+
+                    if let FirstPassNode::Routing(node_id) = node {
+                        if let Some(prev_id) = prev_node_id {
+                            let key = (prev_id, *node_id);
+                            // TODO: Duplicate edges are possible! How do we handle this in CSR?
+                            //   https://www.openstreetmap.org/way/1060404609
+                            //   https://www.openstreetmap.org/way/1060404608
+                            //
+                            // For now, just go with whatever we see first...
+                            if edge_data.contains_key(&key) {
+                                continue;
+                            }
+
+                            edge_data.insert(
                                 key,
-                                // TODO: populate
                                 EdgeData {
                                     dist: accumulated_dist,
                                     tags: strip_tags(&way.tags),
+                                    // TODO: populate
                                     geometry: vec![],
                                 },
                             );
                         }
-                    }
 
-                    prev_node_id = Some(*node_id);
+                        prev_node_id = Some(*node_id);
+                    }
                 }
             }
         }
     }
 
-    let mut edges = edges.keys().collect::<Vec<_>>();
+    let mut edges = edge_data.keys().collect::<Vec<_>>();
     edges.sort();
 
     let csr = Csr::from_sorted_edges(&edges).unwrap();
@@ -266,11 +282,57 @@ fn construct_graph() -> Result<Graph, std::io::Error> {
         csr.edge_count()
     );
 
-    Ok(Graph { csr, node_data })
+    Ok(OsmGraph {
+        csr,
+        node_data,
+        edge_data,
+    })
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let _graph = construct_graph()?;
+    let graph = construct_graph()?;
+
+    for from in 0..100 {
+        for to in 1..100 {
+            let path = astar(
+                &graph.csr,
+                from,
+                |node_id| node_id == to,
+                |edge_ref| {
+                    let key = (edge_ref.source(), edge_ref.target());
+                    let edge = graph.edge_data.get(&key).expect("invalid edge");
+                    edge.dist
+                },
+                |_node_id| {
+                    // TODO: haversine(node, to) * distance multiple?
+                    0
+                },
+            );
+
+            if let Some((cost, ref path)) = path {
+                println!("Path: {:?}", path);
+                let geom = path
+                    .iter()
+                    .map(|node_id| graph.node_data.get(&node_id).expect("invalid from_node_id"))
+                    .map(|node| [node.coord.lon, node.coord.lat])
+                    .collect::<Vec<_>>();
+
+                println!(
+                    "
+{{ \"type\": \"Feature\",
+  \"geometry\": {{
+    \"type\": \"LineString\",
+    \"coordinates\": {:?}
+  }},
+  \"properties\": {{}}
+}}",
+                    geom
+                );
+
+                return Ok(());
+            }
+        }
+    }
 
     Ok(())
 }
