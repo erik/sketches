a502ef53904711e80275ed763e9864d987a14151
Erik Price <github@erikprice.net>
2022-06-16 19:33:32 +0200

    Build abstract syntax tree from grammar

--- src/profile.rs
+++ src/profile.rs
@@ -22,13 +22,8 @@ pub enum Expression {
     Number(f32),
     String(String),
     TagPattern(Vec<TagPattern>),
-    Block(Block),
-}
-
-#[derive(Debug)]
-pub struct WhenClause {
-    condition: Expression,
-    value: Expression,
+    NamedBlock(NamedBlock),
+    WhenBlock(WhenBlock),
 }
 
 #[derive(Debug)]
@@ -36,14 +31,18 @@ pub struct Scope(HashMap<String, Expression>);
 
 #[derive(Debug)]
 pub struct NamedBlock {
-    pub name: String,
-    pub body: Vec<Expression>,
+    scope: Option<Scope>,
+    name: String,
+    body: Vec<Expression>,
 }
 
 #[derive(Debug)]
-pub enum Block {
-    Named(NamedBlock),
-    When(Vec<WhenClause>),
+pub struct WhenBlock(Vec<WhenClause>);
+
+#[derive(Debug)]
+pub struct WhenClause {
+    condition: Expression,
+    value: Expression,
 }
 
 #[derive(Debug)]
@@ -61,30 +60,176 @@ struct ProfileBuilder {
 }
 
 fn parse(source: &str) -> Result<Profile, Error<Rule>> {
-    let syntax_tree = ProfileParser::parse(Rule::top_level, source)?;
-    for pair in syntax_tree {
-        match pair.as_rule() {
-            Rule::profile => {
-                let mut pair = pair.into_inner();
-                let name = parse_str(pair.next().unwrap());
-                let body = parse_block(pair.next().unwrap());
-                println!("name = {:?}. now => {:?}", name, body);
+    let mut syntax_tree = ProfileParser::parse(Rule::top_level, source)?;
+    let root_node = syntax_tree.next().unwrap();
+
+    if root_node.as_rule() != Rule::profile {
+        panic!("unexpected: {:?}", root_node.as_rule());
+    }
+
+    Ok(parse_profile(root_node.into_inner()))
+}
+
+fn parse_profile(mut pairs: pest::iterators::Pairs<Rule>) -> Profile {
+    let profile_name = parse_as_str(pairs.next().unwrap());
+    let mut scope = None;
+    let mut node_cost = None;
+    let mut way_cost = None;
+
+    for node in pairs {
+        match node.as_rule() {
+            Rule::define_block => {
+                scope = Some(parse_define_block(node.into_inner()));
             }
 
-            Rule::EOI => break,
-            rule => panic!("unexpected: {:?}", rule),
+            Rule::named_block => {
+                let block = parse_named_block(node.into_inner());
+                match block.name.as_str() {
+                    "node" => node_cost = Some(block),
+                    "way" => way_cost = Some(block),
+                    name => panic!("unexpected block: {:?}", name),
+                }
+            }
+
+            _ => panic!("unexpected"),
         }
     }
 
-    todo!()
+    Profile {
+        name: profile_name.into(),
+        globals: scope.unwrap(),
+        way_cost: way_cost.unwrap(),
+        node_cost: node_cost.unwrap(),
+    }
 }
 
-fn parse_block(pair: pest::iterators::Pair<Rule>) -> Vec<Expression> {
-    vec![]
+fn parse_define_block(pairs: pest::iterators::Pairs<Rule>) -> Scope {
+    let mut scope = HashMap::new();
+
+    for node in pairs {
+        match node.as_rule() {
+            Rule::assignment => {
+                let mut assignment_node = node.into_inner();
+                let lhs = parse_as_str(assignment_node.next().unwrap());
+                let rhs = parse_expr(assignment_node.next().unwrap());
+
+                scope.insert(lhs.into(), rhs);
+            }
+
+            rule => panic!("unexpected node: {:?}", rule),
+        }
+    }
+
+    Scope(scope)
 }
 
-fn parse_str<'i>(pair: pest::iterators::Pair<'i, Rule>) -> &'i str {
+fn parse_expr(pair: pest::iterators::Pair<Rule>) -> Expression {
     match pair.as_rule() {
+        Rule::bool => Expression::Bool(pair.as_str() == "true"),
+        Rule::number => Expression::Number(pair.as_str().parse().unwrap()),
+        Rule::string => Expression::String(parse_as_str(pair).into()),
+        Rule::tag_expr => Expression::TagPattern(parse_tag_expr(pair.into_inner())),
+        Rule::when_block => Expression::WhenBlock(parse_when_block(pair.into_inner())),
+        Rule::named_block => Expression::NamedBlock(parse_named_block(pair.into_inner())),
+        Rule::ident => Expression::Ident(parse_as_str(pair).into()),
+        Rule::invalid => Expression::Invalid,
+        rule => panic!("unexpected rule: {:?}", rule),
+    }
+}
+
+fn parse_when_block(pairs: pest::iterators::Pairs<Rule>) -> WhenBlock {
+    let mut clauses = vec![];
+
+    for node in pairs {
+        let mut node = node.into_inner();
+
+        let condition = {
+            let node = node.next().unwrap();
+            match node.as_rule() {
+                Rule::else_ => Expression::Bool(true),
+                _ => parse_expr(node),
+            }
+        };
+
+        clauses.push(WhenClause {
+            condition,
+            value: parse_expr(node.next().unwrap()),
+        });
+    }
+
+    WhenBlock(clauses)
+}
+
+fn parse_named_block(mut pairs: pest::iterators::Pairs<Rule>) -> NamedBlock {
+    let name = pairs.next().unwrap().as_str().into();
+    let mut scope = None;
+    let mut body = vec![];
+
+    for node in pairs {
+        match node.as_rule() {
+            Rule::define_block => scope = Some(parse_define_block(node.into_inner())),
+            _ => body.push(parse_expr(node)),
+        }
+    }
+
+    NamedBlock { scope, name, body }
+}
+
+fn parse_tag_expr(pairs: pest::iterators::Pairs<Rule>) -> Vec<TagPattern> {
+    let mut exprs = vec![];
+
+    for node in pairs {
+        let expr = match node.as_rule() {
+            Rule::tag_expr_binary_clause => {
+                let mut node = node.into_inner();
+                let key = parse_as_str(node.next().unwrap());
+                let op = node.next().unwrap();
+                let patterns = node
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .into_iter()
+                    .map(|x| parse_as_str(x).into());
+
+                match op.as_rule() {
+                    Rule::op_eq => TagPattern::OneOf {
+                        key: key.into(),
+                        values: patterns.collect(),
+                    },
+                    Rule::op_neq => TagPattern::NoneOf {
+                        key: key.into(),
+                        values: patterns.collect(),
+                    },
+                    rule => panic!("unexpected rule: {:?}", rule),
+                }
+            }
+
+            Rule::tag_expr_exists_clause => {
+                let mut node = node.into_inner();
+                let key = parse_as_str(node.next().unwrap());
+
+                TagPattern::Exists { key: key.into() }
+            }
+
+            Rule::tag_expr_not_exists_clause => {
+                let mut node = node.into_inner();
+                let key = parse_as_str(node.next().unwrap());
+
+                TagPattern::NotExists { key: key.into() }
+            }
+
+            rule => panic!("unexpected: {:?}", rule),
+        };
+
+        exprs.push(expr);
+    }
+
+    exprs
+}
+
+fn parse_as_str<'i>(pair: pest::iterators::Pair<'i, Rule>) -> &'i str {
+    match pair.as_rule() {
+        Rule::ident => &pair.as_str(),
         Rule::string => {
             let s = &pair.as_str();
             &s[1..s.len() - 1]
@@ -111,13 +256,14 @@ profile "kitchen sink" {
         k = 123
         k = value
         k = [k; !k; k=a; k!=a|b; k="a|b"|c]
+        k = [k !k k=a k!=a|b k="a|b"|c]
     }
 
-    any? { true; // split
-           false }
-    when {
-        [highway=path] => 1
-        else           => when { true => false; false => true }
+    any? { true // split
+           false; when {
+               [highway=path] => true
+               else           => when { true => false; false => true }
+           }
     }
 }
 "#;
@@ -135,11 +281,28 @@ profile "kitchen sink" {
 // Simple test profile
 profile "test" {
     define {
-        base-cost = 123
+        is-wet?     = false
+        valley-mode = when {
+            eq? { hills; 2 } => 80
+            else             => 0
+        }
+        base-cost   = 123
+        this        = true
+        that        = "the other"
+        bar         = [highway=path; access; access!=private]
+    }
+
+    node {
+        sum { 1 2 }
+    }
+
+    way {
+        any? { true false }
     }
 }
 "#;
 
-        parse(input).expect("foo");
+        let profile = parse(input).expect("parse success");
+        assert_eq!(profile.name, "test");
     }
 }
