404a8dc7f53f508c4a2c05edac9e63447ab7a842
Erik Price <github@erikprice.net>
2022-07-09 21:25:07 +0200

    Support routing multiple segments

--- web/js/main.js
+++ web/js/main.js
@@ -1,76 +1,99 @@
-let points = [];
-let map = new mapboxgl.Map({
-  container: 'map',
-  style: 'https://tiles.stadiamaps.com/styles/outdoors.json',
-  center: [12, 53],
-  zoom: 4
-});
+export class MapContainer {
+  constructor(elem) {
+    this.segments = [];
+    this.controlPoints = [];
+    this.lastPoint = null;
 
-map.on('load', () => {
-  map.addSource('points', {
-    type: 'geojson',
-    data: {
+    this.map = new mapboxgl.Map({
+      container: elem,
+      style: 'https://tiles.stadiamaps.com/styles/outdoors.json',
+      center: [12, 53],
+      zoom: 4
+    });
+
+    // Add zoom and rotation controls to the map.
+    this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.on('load', () => this.attachSources());
+    this.map.on('click', (e) => this.handleClick(e));
+  }
+
+  attachSources() {
+    this.map.addSource('points', {
+      type: 'geojson',
+      data: {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: []
+        }
+      }
+    });
+
+    this.map.addLayer({
+      id: 'route-outline',
+      type: 'line',
+      source: 'points',
+      layout: {'line-join': 'round', 'line-cap': 'round'},
+      paint: {'line-color': '#fff', 'line-width': 8}
+    });
+
+    this.map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: 'points',
+      layout: {'line-join': 'round', 'line-cap': 'round'},
+      paint: {'line-color': '#a3f', 'line-width': 3}
+    });
+  }
+
+  addLineSegment(points) {
+    this.segments.push(points);
+
+    const data = {
       type: 'Feature',
       properties: {},
       geometry: {
         type: 'LineString',
-        coordinates: []
+        coordinates: this.segments.flat().map(({lat, lon}) => [lon, lat])
       }
-    }
-  });
+    };
 
-  map.addLayer({
-    id: 'route-outline',
-    type: 'line',
-    source: 'points',
-    layout: {'line-join': 'round', 'line-cap': 'round'},
-    paint: {'line-color': '#fff', 'line-width': 8}
-  });
-
-  map.addLayer({
-    id: 'route',
-    type: 'line',
-    source: 'points',
-    layout: {'line-join': 'round', 'line-cap': 'round'},
-    paint: {'line-color': '#a3f', 'line-width': 3}
-  });
-});
-
-function drawLine(points) {
-  const data = {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'LineString',
-      coordinates: points.map(({lat, lon}) => [lon, lat])
-    }
-  };
-  console.log('Setting data to ', data);
-  map.getSource('points')
-    .setData(data);
-}
-
-// Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl());
-
-map.on('click', (e) => {
-  let {lat, lng} = e.lngLat;
-  points.push({lat, lon: lng});
-  if (points.length === 2) {
-    let [from, to] = points;
-    points = [];
-    fetch('/route', {
-      headers: { 'Content-Type': 'application/json' },
-      method: 'POST',
-      body: JSON.stringify({from, to}),
-    })
-      .then(res => res.json())
-      .then(res => {
-        console.log('res: ', res);
-        if (res.route) {
-          drawLine(res.route);
-        }
-      })
-      .catch(err => console.error(err));
+    console.log('Setting data to ', data);
+    this.map.getSource('points')
+      .setData(data);
   }
-});
+
+  handleClick(e) {
+    let point = {
+      lat: e.lngLat.lat,
+      lon: e.lngLat.lng,
+    };
+
+    let marker = new mapboxgl.Marker()
+        .setLngLat([point.lon, point.lat])
+        .addTo(this.map);
+
+    this.controlPoints.push(marker);
+
+    if (this.lastPoint !== null) {
+      fetch('/route', {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify({
+          from: this.lastPoint,
+          to: point
+        }),
+      })
+        .then(res => res.json())
+        .then(res => {
+          console.log('Route response: ', res);
+          if (res.route) { this.addLineSegment(res.route); }
+          else { marker.remove(); }
+        })
+        .catch(err => console.error(err));
+    }
+
+    this.lastPoint = point;
+  }
+}
