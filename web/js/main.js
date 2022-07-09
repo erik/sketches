f841c03a1908cb9deb50f282006ba1f0654a4561
Erik Price <github@erikprice.net>
2022-07-10 00:41:14 +0200

    Clean up undo handling

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,7 @@
 export class MapContainer {
   constructor(elem) {
     this.segments = [];
-    this.controlPoints = [];
+    this.pendingReq = Promise.resolve();
     this.lastPoint = null;
 
     this.map = new mapboxgl.Map({
@@ -31,6 +31,18 @@ export class MapContainer {
       }
     });
 
+    this.map.addSource('points-pending', {
+      type: 'geojson',
+      data: {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: []
+        }
+      }
+    });
+
     this.map.addLayer({
       id: 'route-outline',
       type: 'line',
@@ -46,6 +58,14 @@ export class MapContainer {
       layout: {'line-join': 'round', 'line-cap': 'round'},
       paint: {'line-color': '#a3f', 'line-width': 3}
     });
+
+    this.map.addLayer({
+      id: 'route-pending',
+      type: 'line',
+      source: 'points-pending',
+      layout: {'line-join': 'round', 'line-cap': 'round'},
+      paint: {'line-color': '#fa3', 'line-width': 3}
+    });
   }
 
   addLineSegment(points) {
@@ -54,18 +74,21 @@ export class MapContainer {
   }
 
   redraw() {
-    const data = {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'LineString',
-        coordinates: this.segments.flat().map(({lat, lon}) => [lon, lat])
-      }
-    };
+    const route = asLineString(
+      this.segments
+        .filter(it => !it.loading)
+        .map(it => it.geometry)
+        .flat()
+    );
+    const pending = asLineString(
+      this.segments
+        .filter(it => it.loading)
+        .map(it => it.geometry)
+        .flat()
+    );
 
-    console.log('Setting data to ', data);
-    this.map.getSource('points')
-      .setData(data);
+    this.map.getSource('points').setData(route);
+    this.map.getSource('points-pending').setData(pending);
   }
 
   handleClick(e) {
@@ -78,45 +101,69 @@ export class MapContainer {
         .setLngLat([point.lon, point.lat])
         .addTo(this.map);
 
-    this.controlPoints.push(marker);
-
     if (this.lastPoint !== null) {
-      fetch('/route', {
-        headers: { 'Content-Type': 'application/json' },
-        method: 'POST',
-        body: JSON.stringify({
-          from: this.lastPoint,
-          to: point
-        }),
+      let segment = {
+        loading: true,
+        from: this.lastPoint,
+        to: point,
+        geometry: [this.lastPoint, point],
+        marker: marker,
+      };
+
+      this.pendingReq.then(() => {
+        this.segments.push(segment);
+        this.redraw();
+
+        return fetch('/route', {
+          headers: { 'Content-Type': 'application/json' },
+          method: 'POST',
+          body: JSON.stringify({
+            from: this.lastPoint,
+            to: point,
+          }),
+        });
       })
         .then(res => res.json())
         .then(res => {
-          console.log('Route response: ', res);
-
+          console.log('routing response', res);
           if (res.route) {
-            this.addLineSegment(res.route);
+            this.lastPoint = point;
+            segment.loading = false;
+            segment.geometry = res.route;
+            this.redraw();
           } else {
-            this.lastPoint = null;
-            marker.remove();
+            this.popSegment();
           }
         })
-        .catch(err => console.error(err));
+        .catch(err => {
+          console.error('something went wrong', err);
+          this.popSegment();
+        });
+    } else {
+      this.lastPoint = point;
     }
-
-    this.lastPoint = point;
   }
 
-  undo() {
-    if (this.controlPoints.length !== 0) {
-      this.controlPoints.pop().remove();
-    }
+  popSegment() {
+    if (this.segments.length === 0) { return; }
 
-    if (this.segments.length !== 0) {
-      this.segments.pop();
-    }
+    const segment = this.segments.pop();
+    segment.marker.remove();
+    this.lastPoint = segment.from;
 
     this.redraw();
   }
+}
+
+function asLineString(points) {
+  return {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'LineString',
+      coordinates: points.map(({lat, lon}) => [lon, lat])
+    }
+  };
 }
 
 class UndoMapControl {
@@ -134,7 +181,7 @@ class UndoMapControl {
   }
 
   onClick() {
-    this._container.undo();
+    this._container.popSegment();
   }
 
 
