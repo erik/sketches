9ee5e77cb08cd3aaa38654676beaa6a1230a1a37
Erik Price <github@erikprice.net>
2022-07-13 19:36:41 +0200

    Rewrite frontend rendering

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,138 @@
+const featureLine = (path, props) => ({
+  type: 'Feature',
+  geometry: {
+    type: 'LineString',
+    coordinates: path.map(it => [it.lng, it.lat]),
+  },
+  properties: props,
+});
+
+const featurePoint = (pt, props) => ({
+  type: 'Feature',
+  geometry: {
+    type: 'Point',
+    coordinates: [pt.lng, pt.lat],
+  },
+  properties: props,
+});
+
+const featureCollection = (features = []) => ({
+  type: 'FeatureCollection',
+  features,
+});
+
+class ControlPoint {
+  static nextId = 0;
+
+  constructor(pt) {
+    this.id = ControlPoint.nextId++;
+    this.point = pt;
+  }
+
+  reposition(pt) {
+    this.point = pt;
+  }
+
+  asGeoJSON() {
+    return featurePoint(this.point, {});
+  }
+}
+
+class Segment {
+  static nextId = 0;
+
+  constructor(from, to) {
+    this.id = Segment.nextId++;
+    this.from = from;
+    this.to = to;
+
+    this.state = 'start';
+    this.geometry = [this.from.point, this.to.point];
+  }
+
+  async fetch() {
+    this.state = 'load';
+
+    const res = await fetch('/route', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({
+        from: {
+          lon: this.from.point.lng,
+          lat: this.from.point.lat,
+        },
+        to: {
+          lon: this.to.point.lng,
+          lat: this.to.point.lat,
+        }
+      }),
+    }).catch(err => {
+        console.error('something went wrong', err);
+        this.state = 'error';
+    });
+
+    const json = await res.json();
+    console.log('routing response', json);
+
+    if (json === null) {
+      this.state = 'no_route';
+      return;
+    }
+
+    this.state = 'done';
+    this.geometry = json.geometry.map(it => ({
+      lng: it.lon,
+      lat: it.lat,
+    }));
+
+    if (this.geometry.length >= 2) {
+      this.from.reposition(this.geometry[0]);
+      this.to.reposition(this.geometry[this.geometry.length - 1]);
+    }
+  }
+
+  splitAt(cp) {
+    return [
+      new Segment(this.from, cp),
+      new Segment(cp, this.to),
+    ];
+  }
+
+  asGeoJSON() {
+    const style = {
+      'start': {
+        color: '#f3a',
+        width: 3,
+      },
+      'load': {
+        color: '#31a',
+        width: 3,
+      },
+      'no_route': {
+        color: '#f21',
+      },
+      'error': {
+        color: '#f21',
+      },
+      'done': {
+        color: '#a3f',
+      }
+    };
+
+    return featureLine(this.geometry, {
+      id: this.id,
+      width: 5,
+      ...style[this.state],
+    });
+  }
+}
+
 export class MapContainer {
   constructor(elem) {
+    this.controlPoints = [];
     this.segments = [];
-    this.pendingReq = null;
-    this.lastPoint = null;
+    this.splitMarker = new mapboxgl.Marker({color: '#fff'});
+    this.hoveredSegment = null;
 
     this.map = new mapboxgl.Map({
       container: elem,
@@ -13,177 +143,159 @@ export class MapContainer {
     });
 
     // Add zoom and rotation controls to the map.
-    this.map.addControl(new mapboxgl.NavigationControl());
-    this.map.addControl(new UndoMapControl(this));
-    this.map.on('load', () => this.attachSources());
-    this.map.on('click', (e) => this.handleClick(e));
+    this.map.addControl(new mapboxgl.NavigationControl())
+      .addControl(new UndoMapControl(this))
+      .on('load', () => this.attachSources())
+      .on('click', (e) => this.handleClick(e))
+    // TODO: refactor this. not really how it's meant to work.
+      .on('mousemove', 'route', (e) => this.handleMouseMove(e))
+      .on('mouseleave', 'route', (e) => this.handleMouseMove(e))
+      .on('mousedown', 'route', (e) => {
+        e.preventDefault();
+        this.map.getCanvasContainer().style.cursor = 'grabbing';
+
+        // FIXME: this is shit
+        const cb = this.handleDragSplit.bind(this);
+        function handler(e) { return cb(e); }
+
+        this.map
+          .on('mousemove', handler)
+          .once('mouseup', (e) => {
+            this.map.off('mousemove', handler);
+            this.handleDragSplitEnd(e);
+          });
+      });
+
   }
 
-  attachSources() {
-    this.map.addSource('points', {
-      type: 'geojson',
-      data: {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-          type: 'LineString',
-          coordinates: []
-        }
-      }
-    });
+  handleDragSplit(e) {
+    if (this.hoveredSegment === null) return;
 
-    this.map.addSource('points-pending', {
-      type: 'geojson',
-      data: {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-          type: 'LineString',
-          coordinates: []
-        }
-      }
-    });
-
-    this.map.addLayer({
-      id: 'route-outline',
-      type: 'line',
-      source: 'points',
-      layout: {'line-join': 'round', 'line-cap': 'round'},
-      paint: {'line-color': '#fff', 'line-width': 8}
-    });
-
-    this.map.addLayer({
-      id: 'route',
-      type: 'line',
-      source: 'points',
-      layout: {'line-join': 'round', 'line-cap': 'round'},
-      paint: {'line-color': '#a3f', 'line-width': 3}
-    });
-
-    this.map.addLayer({
-      id: 'route-pending',
-      type: 'line',
-      source: 'points-pending',
-      layout: {'line-join': 'round', 'line-cap': 'round'},
-      paint: {'line-color': '#fa3', 'line-width': 3}
-    });
+    this.splitMarker.setLngLat(e.lngLat);
   }
 
-  addLineSegment(points) {
-    this.segments.push(points);
-    this.redraw();
+  handleDragSplitEnd(e) {
+    if (this.hoveredSegment === null) return;
+    this.map.getCanvasContainer().style.cursor = '';
+
+    const point = new ControlPoint(e.lngLat);
+    const splitIndex = this.segments.indexOf(this.hoveredSegment);
+
+    this.controlPoints.splice(splitIndex + 1, 0, point);
+
+    this.segments = [
+      ...this.segments.slice(0, splitIndex),
+      ...this.hoveredSegment.splitAt(point),
+      ...this.segments.slice(splitIndex + 1),
+    ];
+
+    this.splitMarker.remove();
+    this.hoveredSegment = null;
+
+    this.reroute();
   }
 
-  redraw() {
-    const route = asLineString(
-      this.segments
-        .filter(it => !it.loading)
-        .map(it => it.geometry)
-        .flat()
-    );
-    const pending = asLineString(
-      this.segments
-        .filter(it => it.loading)
-        .map(it => it.geometry)
-        .flat()
-    );
+  handleMouseMove(e) {
+    if (this.segments.length === 0) return;
 
-    this.map.getSource('points').setData(route);
-    this.map.getSource('points-pending').setData(pending);
-  }
-
-  handleClick(e) {
-    // It's too annoying to have multiple (fallible) requests in flight at once.
-    if (this.pendingReq !== null) {
+    const items = this.map.queryRenderedFeatures(e.point, {layers: ['route-outline']});
+    if (items.length === 0) {
+      this.splitMarker.remove();
+      this.hoveredSegment = null;
       return;
     }
 
-    const point = {
-      lat: e.lngLat.lat,
-      lon: e.lngLat.lng,
-    };
+    this.hoveredSegment = this.segments.find(it => it.id === items[0].properties.id);
 
-    let marker = new mapboxgl.Marker()
-        .setLngLat([point.lon, point.lat])
-        .addTo(this.map);
+    this.splitMarker
+      .setLngLat(e.lngLat)
+      .addTo(this.map);
+  }
 
-    if (this.lastPoint !== null) {
-      let segment = {
-        loading: true,
-        from: this.lastPoint,
-        to: point,
-        geometry: [this.lastPoint, point],
-        marker: marker,
-      };
-
-      this.pendingReq = Promise.resolve().then(() => {
-        this.segments.push(segment);
-        this.redraw();
-
-        return fetch('/route', {
-          headers: { 'Content-Type': 'application/json' },
-          method: 'POST',
-          body: JSON.stringify({
-            from: this.lastPoint,
-            to: point,
-          }),
-        });
+  attachSources() {
+    this.map
+      .addSource('control-points', { type: 'geojson', data: featureCollection() })
+      .addSource('segments', { type: 'geojson', data: featureCollection() })
+      .addLayer({
+        id: 'route-outline',
+        type: 'line',
+        source: 'segments',
+        layout: {'line-join': 'round', 'line-cap': 'round'},
+        paint: {'line-color': '#fff', 'line-width': 12}
       })
-        .then(res => res.json())
-        .then(res => {
-          console.log('routing response', res);
-
-          if (res === null) {
-            this.popSegment();
-            return;
-          }
-
-          this.lastPoint = res.geometry[res.geometry.length - 1];
-
-          segment.loading = false;
-          segment.geometry = res.geometry;
-          segment.marker
-            .setLngLat(this.lastPoint)
-            .setPopup(new mapboxgl.Popup().setHTML(`
-                  <ul>
-                    <li>distance: ${res.distance_meters} meters</li>
-                    <li>cost    : ${res.route_cost}</li>
-                  </ul>`));
-          this.redraw();
-        })
-        .catch(err => {
-          console.error('something went wrong', err);
-          this.popSegment();
-        })
-        .finally(() => {
-          this.pendingReq = null;
-        });
-
-    } else {
-      this.lastPoint = point;
-    }
+      .addLayer({
+        id: 'route',
+        type: 'line',
+        source: 'segments',
+        layout: {'line-join': 'round', 'line-cap': 'round'},
+        paint: {
+          'line-color': ['get', 'color'],
+          'line-width': ['get', 'width'],
+        }
+      })
+      .addLayer({
+        id: 'control-points',
+        type: 'circle',
+        source: 'control-points',
+        paint: {
+          'circle-color': '#fff',
+          'circle-stroke-color': '#a3f',
+          'circle-stroke-width': 6,
+          'circle-radius': 6,
+        }
+      });
+    ;
   }
 
-  popSegment() {
-    if (this.segments.length === 0) { return; }
+  setData() {
+    const controlPoints = featureCollection(this.controlPoints.map(it => it.asGeoJSON()));
+    const segments = featureCollection(this.segments.map(it => it.asGeoJSON()));
 
-    const segment = this.segments.pop();
-    segment.marker.remove();
-    this.lastPoint = segment.from;
-
-    this.redraw();
+    this.map.getSource('control-points')
+      .setData(controlPoints);
+    this.map.getSource('segments')
+      .setData(segments);
   }
-}
 
-function asLineString(points) {
-  return {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'LineString',
-      coordinates: points.map(({lat, lon}) => [lon, lat])
+  reroute() {
+    this.setData();
+
+    const promises = this.segments
+          .filter(it => it.state === 'start')
+          .map(it => it.fetch());
+
+    Promise.allSettled(promises)
+      .then(() => this.setData());
+  }
+
+  handleClick(e) {
+    const cp = new ControlPoint(e.lngLat);
+    this.pushControlPoint(cp);
+  }
+
+  pushControlPoint(cp) {
+    this.controlPoints.push(cp);
+    if (this.controlPoints.length > 1) {
+      const from = this.controlPoints[this.controlPoints.length - 2];
+      const segment = new Segment(from, cp);
+
+      this.pushSegment(segment);
     }
-  };
+    this.reroute();
+  }
+
+  pushSegment(segment) {
+    this.segments.push(segment);
+  }
+
+  popControlPoint() {
+    if (this.controlPoints.length === 0) return;
+
+    this.controlPoints.pop();
+    if (this.segments.length > 0) this.segments.pop();
+
+    this.reroute();
+  }
 }
 
 class UndoMapControl {
@@ -201,7 +313,7 @@ class UndoMapControl {
   }
 
   onClick() {
-    this._container.popSegment();
+    this._container.popControlPoint();
   }
 
 
